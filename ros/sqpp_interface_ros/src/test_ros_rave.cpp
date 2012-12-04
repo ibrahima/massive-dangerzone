a4ba93964bacd3965682f4c35f4aad3c51a6593a
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2012-12-03 17:07:18 -0800

    Test executable linking against libs (empty at this point, but it builds).

---
+++ ros/sqpp_interface_ros/src/test_ros_rave.cpp
@@ -0,0 +1,17 @@
+#include <sqpp_interface_ros/sqpp_interface_ros.h>
+#include "simulation/util.h"
+#include "sqp/traj_sqp.h"
+#include "sqp/planning_problems2.h"
+#include <moveit/kinematic_state/conversions.h>
+#include <Eigen/Dense>
+#include <sqpp_interface_ros/rosconversions.h>
+#include <iostream>
+
+namespace sqpp_interface_ros
+{
+  
+}
+
+int main(int argc, const char* argv[]){
+  cout << "foo" << endl;
+}
