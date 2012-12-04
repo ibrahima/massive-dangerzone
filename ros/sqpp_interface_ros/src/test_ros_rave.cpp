f4965e2609c1ffd7b4055ec3a7459dc86d2b8180
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2012-12-03 21:57:58 -0800

    Include ALL the headers. (required messing with log4cplus build, I hope the output is in the right place - compiles so far though).

--- ros/sqpp_interface_ros/src/test_ros_rave.cpp
+++ ros/sqpp_interface_ros/src/test_ros_rave.cpp
@@ -5,6 +5,22 @@
 #include <moveit/kinematic_state/conversions.h>
 #include <Eigen/Dense>
 #include <sqpp_interface_ros/rosconversions.h>
+
+#include "simulation/simplescene.h"
+#include "simulation/config_bullet.h"
+#include "robots/robot_manager.h"
+#include "simulation/bullet_io.h"
+#include <boost/foreach.hpp>
+#include "utils/vector_io.h"
+#include "utils/clock.h"
+#include <osg/Depth>
+#include <boost/filesystem.hpp>
+#include "utils/my_exceptions.h"
+#include "sqp/kinematics_utils.h"
+#include "sqp/plotters.h"
+#include "sqp/traj_sqp.h"
+#include "sqp/planning_problems2.h"
+
 #include <iostream>
 
 namespace sqpp_interface_ros
